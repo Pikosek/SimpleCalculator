55896daa89384e74f15c3967f13026620d8bb4db
Pikosek <pwqbwyre@prokonto.pl>
2023-02-03 10:48:48 +0100

    SimpleCalculator simple mechanics

--- main.java
+++ main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class main {
         public static void main(String[] args){
             System.out.println("KAlkulator v1.0");
+            Scanner s = new Scanner(System.in);
+            int firstNumber = s.nextInt();
+            int secondNUmber = s.nextInt();
+
+            int sum = firstNumber + secondNUmber;
+            System.out.println(sum);
+
+            System.out.print("test");
+            System.out.print("eloelo");
         }
 }
